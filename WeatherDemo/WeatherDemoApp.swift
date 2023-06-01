ad846ed596509fc14a73847965b0ef20958f36ab
Alley Pereira <noyallepereira@gmail.com>
2023-05-31 21:48:45 -0300

    wip: Weather Welcome View

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by userext on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
